4d49b13683cfaf8bdd63582e3fec51546ca1a498
mr cmuc <github@nextcos.de>
2023-12-11 11:49:08 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,20 +21,7 @@ export default async function RootLayout ({
 
   return (
     <html lang="en">
-    <head>
-      <Script id="gtag">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-PRPSXZS');`}</Script>
-      <link rel="icon" type="image/png" href={currentUser!.avatar_url} />
-      <title>{`${currentUser!.login}'s Dashboard | OSSInsight lite`}</title>
-    </head>
     <body>
-    <noscript>
-      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PRPSXZS"
-              height="0" width="0" style={{ display: 'none', visibility: 'hidden' }} />
-    </noscript>
     <App currentUser={currentUser!}>
       <Suspense fallback={<AppLoading />}>
         {children}
